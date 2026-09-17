7742ed750133a223a09b5119aaba03c084ef5251
Taiwo Ademola Sunday <taiwoademola065@gmail.com>
2026-09-17 11:20:24 +0100

    Refactor HTML structure and improve voting eligibility display messages in practice.js

--- practice.js
+++ practice.js
@@ -8,13 +8,19 @@
         let currentYear = new Date().getFullYear()
         let age = currentYear - birthDate;
         if(age >= 18){
-          console.log('vote dey go jor');
-          displayArea.innerHTML = `<p>enjoy voting</p>`
+          // console.log('vote dey go jor');
+          displayArea.innerHTML = `<p class="text-success">You are eligible to vote, carry on</p>`
           
         }else{
-          console.log('grow up');
+          // console.log('grow up');
+          displayArea.innerHTML = `<p class="text-danger">You are not eligible to vote, grow up fast you have ${18 - age}years to vote</p>`
           
         }
 
       }
+    }
+
+
+    if(gpa == 4.5 && gpa == 5.0){
+
     }
